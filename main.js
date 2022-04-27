d99c1d27a45cf87822f94080ce25882e35e59851
CodingExpert123 <83323361+CodingExpert123@users.noreply.github.com>
2022-04-26 17:25:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(a);
+console.log(b);
+
+console.log(add);
+console.log(first_name.concat(last_name));
